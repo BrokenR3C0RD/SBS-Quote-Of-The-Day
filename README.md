9e7f5875da21df11efc89da1aca22268411cff28
BrokenR3C0RD <codyeksal@gmail.com>
2016-11-03 17:47:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Quote of the Day
+================
+
+This repo is for storing text used for the Quote of the Day on my account on SmileBASIC Source.
---
+++ quotes.js
@@ -0,0 +1,66 @@
+// QotD file parser
+// By BrokenR3C0RD
+
+var fs = require("fs");
+
+const PARSER_RE = /\s*(?:\#\|\s*(Date|From|Description)\:\s*(.*)|\#.*|\-\s(.+)|(.+)|)/i;
+
+/*
+      Parser fields:
+      =============
+   1, 2: Meta data field, value
+      3: Person quoted
+      4: Quote data
+*/
+
+var parse = function(file){
+  var data = fs.readFileSync(file, "utf8");
+  var tmp = {
+    meta:   {},
+    quotes: []
+  };
+
+  var done = false;
+
+  var quote = {
+    quote: "",
+    from:  ""
+  };
+
+  var out = [];
+
+  data.split("\n").forEach(function(line){
+    var data = PARSER_RE.exec(line.trim());
+
+    if(data == null)
+      throw new Error("Something is wrong with the parser! It didn't handle a string properly!");
+
+    if(data[1] != null){
+      if(done){
+        out.push(tmp);
+        tmp = {
+          meta:   {},
+          quotes: []
+        };
+        done = false;
+      }
+      var field = data[1].toLowerCase();
+      tmp.meta[field] = data[2].trim();
+    } else if(data[3] != null && quote.quote !== ""){
+      quote.from = data[3];
+      quote.quote = quote.quote.trim();
+      tmp.quotes.push(quote);
+      quote = {
+        quote: "",
+        from:  ""
+      };
+    } else if(data[4] != null){
+      done = true;
+      quote.quote += data[4] + "\n";
+    }
+  });
+  out.push(tmp);
+  return out;
+};
+
+module.exports = parse;
